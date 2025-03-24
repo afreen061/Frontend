dc1bd8744f8af7409b504e9fbc3494bb5477e44c
afreen061 <afreenbismillah786@gmail.com>
2025-03-24 17:09:54 +0530

    3 comonth

---
+++ hello.js
@@ -0,0 +1,23 @@
+let x =6;
+let s=                             "                            lll    f     fr"
+console.log(x);
+console.log(s);
+let typea = 2 + 5 + "5" +2;// 7(2+7) added as number befron start of string 
+let typeb = 4+ "5" + 2 + 3; //4523 as before no arimetic operand before start of string 
+console.log(typeof(typea)) // String 
+console.log(typeof(typeb)) //string 
+
+//var ,let ,const 
+//hoisting var
+hoist = "hello"
+var hoist 
+console.log(hoist) // hello
+//hoisting let
+// hoistlet = "hellolet"
+// let hoistlet
+// console.log(hoistlet) // ReferenceError
+
+
+
+
+

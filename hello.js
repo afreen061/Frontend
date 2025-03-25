606404e5892ee2873a94292845e4498640d871b0
afreen061 <afreenbismillah786@gmail.com>
2025-03-25 17:14:47 +0530

    project

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ console.log(x);
 console.log(s);
 let typea = 2 + 5 + "5" +2;// 7(2+7) added as number befron start of string 
 let typeb = 4+ "5" + 2 + 3; //4523 as before no arimetic operand before start of string 
-console.log(typeof(typea)) // String 
+console.log(typeof(typea)) // String type 
 console.log(typeof(typeb)) //string 
 
 //var ,let ,const 
@@ -16,6 +16,11 @@ console.log(hoist) // hello
 // hoistlet = "hellolet"
 // let hoistlet
 // console.log(hoistlet) // ReferenceError
+
+//hoisting const 
+//  hoistconst = "const";
+// const hoistconst; // Missing initializer
+// console.log(hoistconst)
 
 
 
